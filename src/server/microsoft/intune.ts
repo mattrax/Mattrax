245f6c2e0b5920b1a74187198411143b725b6cca
Oscar Beaumont <oscar@otbeaumont.me>
2024-01-14 20:21:00 +0800

    More intune API endpoints

--- src/server/microsoft/intune.ts
+++ src/server/microsoft/intune.ts
@@ -3,3 +3,32 @@ import { authenticatedFetch } from "./auth";
 
 export const getDevices = () =>
   authenticatedFetch<Device>("/deviceManagement/managedDevices");
+
+export const getApplications = (deviceId: string) =>
+  authenticatedFetch<void>(`/deviceManagement/mobileApps`);
+
+// TODO: Create application
+
+// TODO: Delete application
+
+export const rebootDevice = (deviceId: string) =>
+  authenticatedFetch<void>(
+    `/deviceManagement/managedDevices/${deviceId}/rebootNow`
+  );
+
+export const shutdownDevice = (deviceId: string) =>
+  authenticatedFetch<void>(
+    `/deviceManagement/managedDevices/${deviceId}/shutDown`
+  );
+
+export const syncDevice = (deviceId: string) =>
+  authenticatedFetch<void>(
+    `/deviceManagement/managedDevices/${deviceId}/syncDevice`
+  );
+
+// TODO: Unenroll device
+
+// TODO: Get IOS policy
+// TODO: Create IOS policy
+// TODO: Update IOS policy
+// TODO: Delete IOS policy
