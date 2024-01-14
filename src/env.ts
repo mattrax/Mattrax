e1d403336ace687f48391b57ccdd9fe900c881a5
Oscar Beaumont <oscar@otbeaumont.me>
2024-01-14 11:05:58 +0800

    Microsoft Graph SDK

--- src/env.ts
+++ src/env.ts
@@ -4,9 +4,15 @@ import { validateEnv } from "./server/validateEnv";
 const env = validateEnv(
   object({
     SESSION_SECRET: string([]),
+    MSFT_CLIENT_ID: string([]),
+    MSFT_CLIENT_SECRET: string([]),
+    MSFT_ADMIN_TENANT: string([]),
   }),
   {
     SESSION_SECRET: process.env.SESSION_SECRET,
+    MSFT_CLIENT_ID: process.env.MSFT_CLIENT_ID,
+    MSFT_CLIENT_SECRET: process.env.MSFT_CLIENT_SECRET,
+    MSFT_ADMIN_TENANT: process.env.MSFT_ADMIN_TENANT,
   }
 );
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,40 @@
 import { cache, createAsync } from "@solidjs/router";
 import { For } from "solid-js";
 import { getSession } from "~/server/session";
+import { Client } from "@microsoft/microsoft-graph-client";
+import {
+  TokenCredentialAuthenticationProvider,
+  TokenCredentialAuthenticationProviderOptions,
+} from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
+import { env } from "~/env";
+import { ClientSecretCredential } from "@azure/identity";
 
 const demo = async (deviceId: string) => {
   "use server";
-  return "Hello From The Server!";
+
+  const tokenCredential = new ClientSecretCredential(
+    env.MSFT_ADMIN_TENANT,
+    env.MSFT_CLIENT_ID,
+    env.MSFT_CLIENT_SECRET
+  );
+
+  const client = Client.initWithMiddleware({
+    debugLogging: true,
+    authProvider: new TokenCredentialAuthenticationProvider(tokenCredential, {
+      scopes: ["https://graph.microsoft.com/.default"],
+      getTokenOptions: {},
+    }),
+  });
+
+  try {
+    let userDetails = await client.api("/me").get();
+    console.log(userDetails);
+
+    return "Hello From The Server!";
+  } catch (error) {
+    console.error(error);
+    return error.toString();
+  }
 };
 
 // TODO: Proper input validation using Valibot
@@ -58,6 +88,8 @@ export default function Home() {
       <button onClick={() => demo("hello").then(console.log)}>
         Test Server Actions
       </button>
+
+      <a href="/profile.mobileconfig">Enroll Profile</a>
     </main>
   );
 }
