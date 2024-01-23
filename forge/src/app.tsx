37367faeae90fe0bd90da181263f5223b4a156cd
Oscar Beaumont <oscar@otbeaumont.me>
2024-01-23 14:36:52 +0800

    Add `solid-sonner`

--- forge/src/app.tsx
+++ forge/src/app.tsx
@@ -1,8 +1,10 @@
 // @refresh reload
 import { Router } from "@solidjs/router";
 import { ErrorBoundary, Suspense } from "solid-js";
-import "./app.css";
 import { routes } from "./routes";
+import { Toaster } from "solid-sonner";
+import "./app.css";
+import "./sonner.css";
 
 export default function App() {
   return (
@@ -30,6 +32,7 @@ export default function App() {
         );
       }}
     >
+      <Toaster />
       <Router root={(props) => <Suspense>{props.children}</Suspense>}>
         {routes}
       </Router>
