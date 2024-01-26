6e7fd74209db09b4db4bcd75d52691bf07849320
Oscar Beaumont <oscar@otbeaumont.me>
2024-01-26 10:35:54 +0800

    A bitta ShadCN

--- forge/src/utils/index.ts
+++ forge/src/utils/index.ts
@@ -1,4 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 import { hc } from "hono/client";
 import { AppType } from "~/routes/api/[...api]";
 
 export const client = hc<AppType>("");
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
