e436021084bd9ea77c91143e3dc8573b6d082d1f
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-06 03:43:21 +0800

    It couldn't

--- api/src/stripe.ts
+++ api/src/stripe.ts
@@ -2,5 +2,6 @@ import Stripe from "stripe";
 import { env } from "./env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
   httpClient: Stripe.createFetchHttpClient(),
 });
