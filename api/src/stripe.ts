ba48cbe337dad4a7200a71ea7ae3812f8b363e17
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-06 15:16:06 +0800

    fuck it

--- api/src/stripe.ts
+++ api/src/stripe.ts
@@ -1,4 +1,5 @@
-import Stripe from "stripe";
+// @ts-expect-error // TODO: Fix this properly cause we go no types :(
+import Stripe from "stripe-cloudflare";
 import { env } from "./env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
