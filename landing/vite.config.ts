8ed83df9b644878a21e1a5ae87d90940224514a7
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-06 11:04:55 +0800

    disable SSR on landing

--- landing/vite.config.ts
+++ landing/vite.config.ts
@@ -5,6 +5,8 @@ import pkg from "@vinxi/plugin-mdx";
 const { default: mdx } = pkg;
 export default defineConfig({
   start: {
+    // Be careful enabling this. It was broke in prod so I disabled it.
+    ssr: false,
     extensions: ["mdx", "md"],
   },
   plugins: [
