31befa212dbdc8e37ccacaa91caa247864d75826
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-10 13:49:48 +0800

    fix more stuff

--- aws/setup.sh
+++ aws/setup.sh
@@ -6,5 +6,5 @@
 # TODO: SSH via Tailscale/ZeroTier
 
 ssh-keygen -t ed25519 -a 200 -C "gh-actions"
-cat .ssh/id_ed25519.pub > .ssh/authorized_keys
+cat .ssh/id_ed25519.pub >> .ssh/authorized_keys
 cat .ssh/id_ed25519 # Copy and paste into GH secrets as `SSH_KEY`
