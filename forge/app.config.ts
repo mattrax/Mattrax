79be8ee6d6c98ef68ea24039cc7a63c16778ea54
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-11 02:07:12 +0800

    more patching

--- forge/app.config.ts
+++ forge/app.config.ts
@@ -74,17 +74,29 @@ export default createApp({
 // TODO: Remove this hackery
 
 const workerCode = path.join("dist", "_worker.js", "index.js");
+const routesJson = path.join("dist", "_routes.json");
 process.on("exit", () => {
   if (!fs.existsSync(workerCode)) {
     console.warn("Skipping Cloudflare env patching...");
     return;
   }
 
+  // Cloudflare doesn't allow access to env outside the handler.
+  // So we ship the env with the worker code.
   fs.writeFileSync(
     workerCode,
     `const process={env:${JSON.stringify(
       process.env
     )}};globalThis.process=process.env;${fs.readFileSync(workerCode)}`
+  );
+
+  // Replace Nitro's config so Cloudflare will serve the HTML from the CDN instead of the worker (they can do "304 Not Modified" & ETag caching).
+  fs.writeFileSync(
+    routesJson,
+    JSON.stringify({
+      version: 1,
+      include: ["/api/*"],
+    })
   );
 });
 
