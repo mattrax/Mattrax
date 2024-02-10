ab129ead7d56f71c0025960e1d810a5caabc8174
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-11 00:36:07 +0800

    debug

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,33 +1,33 @@
 // Side-side exports (we don't want these shipped to the client)
 
 import { Hono } from "hono";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+// import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
-import { appRouter } from "./root";
-import { createTRPCContext } from "./trpc";
+// import { appRouter } from "./root";
+// import { createTRPCContext } from "./trpc";
 import { HonoEnv } from "./types";
-import { webhookRouter } from "./routes/webhook";
-import { enrollmentRouter } from "./routes/enrollment";
-import { msRouter } from "./routes/ms";
+// import { webhookRouter } from "./routes/webhook";
+// import { enrollmentRouter } from "./routes/enrollment";
+// import { msRouter } from "./routes/ms";
 
 export const app = new Hono<HonoEnv>()
   .basePath("/api")
   .get("/", (c) => c.json({ message: "Mattrax Forge!" }))
-  .all("/trpc/*", (c) =>
-    fetchRequestHandler({
-      endpoint: "/api/trpc",
-      req: c.req as any,
-      router: appRouter,
-      createContext: (opts) =>
-        createTRPCContext({
-          session: c.env.session,
-          tenantId: c.req.header("x-tenant-id"),
-        }),
-    })
-  )
-  .route("/enrollment", enrollmentRouter)
-  .route("/webhook", webhookRouter)
-  .route("/ms", msRouter)
+  // .all("/trpc/*", (c) =>
+  //   fetchRequestHandler({
+  //     endpoint: "/api/trpc",
+  //     req: c.req as any,
+  //     router: appRouter,
+  //     createContext: (opts) =>
+  //       createTRPCContext({
+  //         session: c.env.session,
+  //         tenantId: c.req.header("x-tenant-id"),
+  //       }),
+  //   })
+  // )
+  // .route("/enrollment", enrollmentRouter)
+  // .route("/webhook", webhookRouter)
+  // .route("/ms", msRouter)
   .all("*", (c) => {
     c.status(404);
     if (c.req.raw.headers.get("Accept")?.includes("application/json")) {
