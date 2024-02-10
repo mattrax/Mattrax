04bc9a5a3fbf2b49753cac417f6885cccae40d5c
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-11 00:54:57 +0800

    reenable `env` + fixed cursed code

--- forge/src/routes/api/getServerSession.ts
+++ forge/src/routes/api/getServerSession.ts
@@ -1,10 +1,10 @@
-import { type GetSessionResult } from "@mattrax/api";
+import { type GetSessionResult, env } from "@mattrax/api";
 import { type H3Event, useSession } from "vinxi/server";
 
 export const getServerSession = async (event: H3Event) => {
   const session = await useSession(event, {
     name: "s",
-    password: "todo", // env.AUTH_SECRET, // TODO
+    password: env.AUTH_SECRET,
     cookie: {
       // Safari gets unhappy
       secure: !import.meta.env.DEV,
