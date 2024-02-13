90db848a283b1403d22070761abbfdcc351a3de0
Brendan Allan <brendonovich@outlook.com>
2024-02-13 17:40:50 +1100

    no ts in build

--- apps/email/next.config.mjs
+++ apps/email/next.config.mjs
@@ -4,6 +4,10 @@ if (process.env.NODE_ENV === "development")
   configDotenv({ path: "../../.env" });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
