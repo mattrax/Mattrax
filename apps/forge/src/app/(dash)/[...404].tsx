3ea94130a54f3d82a75a554d54a08f1edde4e61c
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-15 23:03:43 +0800

    slightly better 404 page

--- apps/forge/src/app/(dash)/[...404].tsx
+++ apps/forge/src/app/(dash)/[...404].tsx
@@ -1,7 +1,2 @@
-export default function NotFound() {
-  return (
-    <div class="p-4">
-      <h1>404 Not Found</h1>
-    </div>
-  );
-}
+import Page from "../[...404]";
+export default Page;
--- apps/forge/src/app/(dash)/[tenant]/[...404].tsx
+++ apps/forge/src/app/(dash)/[tenant]/[...404].tsx
@@ -1,4 +1,2 @@
-// This 404 page is really important. It ensures the `useParams` hook has `tenant` as a property so the sidebar buttons don't go disabled.
-
 import Page from "../[...404]";
 export default Page;
