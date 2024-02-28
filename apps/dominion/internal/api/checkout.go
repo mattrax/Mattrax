b9b9479207f8734e663b31b20fcedf0775e8f6e7
Oscar Beaumont <oscar@otbeaumont.me>
2024-02-29 00:55:40 +0800

    Go smells

--- apps/dominion/internal/api/checkout.go
+++
@@ -1,8 +0,0 @@
-package api
-
-// Checkout removes a device from the management server when its unenrollment is requested.
-func (api *Service) Checkout(deviceId string) error {
-	// TODO: do stuff
-
-	return nil
-}
