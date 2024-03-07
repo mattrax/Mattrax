972a44e970f999edb78b8441bc8101fa36c48ec7
Brendan Allan <brendonovich@outlook.com>
2024-03-07 21:18:40 +0800

    fix page height for sticky navbar

--- apps/forge/tailwind.config.cjs
+++ apps/forge/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {
       colors: {
