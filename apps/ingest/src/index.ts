b32f7c1f65e7290a91f20e14b8fd399afadf5edd
Oscar Beaumont <oscar@otbeaumont.me>
2024-03-23 12:34:08 +0800

    wip: codegen schema for Windows

--- apps/ingest/src/index.ts
+++ apps/ingest/src/index.ts
@@ -36,23 +36,44 @@ function handleNode(node: any, path_prefix: string, results: any[]) {
 
 	const datatype = Object.keys(node.DFProperties[0].DFFormat[0])[0];
 
-	results.push({
-		name,
-		type: "windows",
-		uri: path,
-		description: node.DFProperties[0].Description?.[0] ?? null,
-		datatype, // TODO: Unify this with macOS
-		default: node.DFProperties[0].DefaultValue?.[0] ?? null,
-		scope: path.startsWith("./Device") ? "device" : "user",
+	// results.push({
+	// 	name,
+	// 	type: "windows",
+	// 	uri: path,
+	// 	description: node.DFProperties[0].Description?.[0] ?? null,
+	// 	datatype, // TODO: Unify this with macOS
+	// 	default: node.DFProperties[0].DefaultValue?.[0] ?? null,
+	// 	scope: path.startsWith("./Device") ? "device" : "user",
 
-		// TODO: Possible values -> Can enums be properly represented???
+	// 	// TODO: Possible values -> Can enums be properly represented???
 
-		// TODO: Does this policy require user or device scope???
-		// TODO: Valid operations on it
-	});
+	// 	// TODO: Does this policy require user or device scope???
+	// 	// TODO: Valid operations on it
+	// });
+
+	let type = "()";
+	if (datatype === "bool") {
+		type = "bool";
+	} else if (datatype === "int") {
+		type = "u64";
+	} else if (datatype === "chr") {
+		type = "String";
+	} else {
+		// TODO: Error
+	}
+
+	console.log(datatype);
+
+	const comment = node.DFProperties[0].Description?.[0]
+		? `/// ${node.DFProperties[0].Description?.[0].replace(
+				"\n",
+				"\n\t///",
+		  )}\n\t`
+		: "";
+	results.push(`\t${comment}#[serde(rename = "${path}")] ${name}(${type})`);
 }
 
-async function parseDDF(name: string, description: string, raw: string) {
+async function parseDDF(name: string, raw: string) {
 	const result: any = await new Promise((resolve, reject) => {
 		parseString(raw, (err, result) => {
 			if (err) {
@@ -63,90 +84,168 @@ async function parseDDF(name: string, description: string, raw: string) {
 		});
 	});
 
-	const properties = [];
+	const parts = [];
 	for (const node of result.MgmtTree.Node) {
-		handleNode(node, "", properties);
+		handleNode(node, "", parts);
 	}
 
-	return {
-		name,
-		description,
-		properties,
-	};
+	return parts;
 }
 
-const schema = {
-	generated_at: new Date().toISOString().substring(0, 10),
-	// Intentionally skipped: DevDetail_DDF.xml, DeviceManageabilityDDF.xml, DeviceStatusDDF.xml
-	sections: [
-		await parseDDF(
-			"Policy",
-			"Policy settings for Windows devices.",
-			fs.readFileSync(path.join(__dirname, "../ddf/PolicyDDF_all.xml"), "utf8"),
+const parts = [
+	...(await parseDDF(
+		"Policy",
+		fs.readFileSync(path.join(__dirname, "../ddf/PolicyDDF_all.xml"), "utf8"),
+	)),
+	...(await parseDDF(
+		"ActiveSync",
+		fs.readFileSync(
+			path.join(__dirname, "../ddf/ActiveSyncCSP_DDF.xml"),
+			"utf8",
 		),
-		await parseDDF(
-			"ActiveSync",
-			"TODO",
-			fs.readFileSync(
-				path.join(__dirname, "../ddf/ActiveSyncCSP_DDF.xml"),
-				"utf8",
-			),
+	)),
+	...(await parseDDF(
+		"AssignedAccess",
+		fs.readFileSync(
+			path.join(__dirname, "../ddf/AssignedAccessDDF.xml"),
+			"utf8",
 		),
-		await parseDDF(
-			"CertificateStore",
-			"TODO",
-			fs.readFileSync(
-				path.join(__dirname, "../ddf/CertificateStore_DDF.xml"),
-				"utf8",
-			),
+	)),
+	...(await parseDDF(
+		"CertificateStore",
+		fs.readFileSync(
+			path.join(__dirname, "../ddf/CertificateStore_DDF.xml"),
+			"utf8",
 		),
-		await parseDDF(
-			"ClientCertificateInstall",
-			"TODO",
-			fs.readFileSync(
-				path.join(__dirname, "../ddf/ClientCertificateInstall_DDF.xml"),
-				"utf8",
-			),
+	)),
+	...(await parseDDF(
+		"ClientCertificateInstall",
+		fs.readFileSync(
+			path.join(__dirname, "../ddf/ClientCertificateInstall_DDF.xml"),
+			"utf8",
 		),
-		// TODO: CMPolicyEnterprise_DDF
-		await parseDDF(
-			"Defender",
-			"TODO",
-			fs.readFileSync(path.join(__dirname, "../ddf/DefenderDDF.xml"), "utf8"),
+	)),
+	...(await parseDDF(
+		"CMPolicyEnterprise",
+		fs.readFileSync(
+			path.join(__dirname, "../ddf/CMPolicyEnterprise_DDF.xml"),
+			"utf8",
 		),
-		// TODO: ...
-		await parseDDF(
-			"Win32AppInventory",
-			"TODO",
-			fs.readFileSync(
-				path.join(__dirname, "../ddf/Win32AppInventoryDDF.xml"),
-				"utf8",
-			),
-		),
-		// await parseDDF(
-		// 	"WindowsLicensingDDF",
-		// 	"TODO",
-		// 	fs.readFileSync(
-		// 		path.join(__dirname, "../ddf/WindowsLicensingDDF.XML"),
-		// 		"utf8",
-		// 	),
-		// ),
-		// await parseDDF(
-		// 	"WindowsSecurityAuditing",
-		// 	"TODO",
-		// 	fs.readFileSync(
-		// 		path.join(__dirname, "../ddf/WindowsSecurityAuditing_DDF.xml"),
-		// 		"utf8",
-		// 	),
-		// ),
-	],
-};
+	)),
+	...(await parseDDF(
+		"Defender",
+		fs.readFileSync(path.join(__dirname, "../ddf/DefenderDDF.xml"), "utf8"),
+	)),
+	// 		// TODO: ...
+	// 		await parseDDF(
+	// 			"Win32AppInventory",
+	// 			"TODO",
+	// 			fs.readFileSync(
+	// 				path.join(__dirname, "../ddf/Win32AppInventoryDDF.xml"),
+	// 				"utf8",
+	// 			),
+	// 		),
+	// 		// await parseDDF(
+	// 		// 	"WindowsLicensingDDF",
+	// 		// 	"TODO",
+	// 		// 	fs.readFileSync(
+	// 		// 		path.join(__dirname, "../ddf/WindowsLicensingDDF.XML"),
+	// 		// 		"utf8",
+	// 		// 	),
+	// 		// ),
+	// 		// await parseDDF(
+	// 		// 	"WindowsSecurityAuditing",
+	// 		// 	"TODO",
+	// 		// 	fs.readFileSync(
+	// 		// 		path.join(__dirname, "../ddf/WindowsSecurityAuditing_DDF.xml"),
+	// 		// 		"utf8",
+	// 		// 	),
+	// 		// ),
+];
 
-const schemaPath = path.resolve(__dirname, "../src/schema.json");
-const numConfigurations = schema.sections
-	.map((s) => s.properties.length)
-	.reduce((a, b) => a + b, 0);
-console.log(
-	`Saved schema with ${numConfigurations} configurations to '${schemaPath}'`,
-);
-fs.writeFileSync(schemaPath, JSON.stringify(schema, null, 2));
+const result = `#[derive(Debug, Clone, Serialize, Deserialize, Type)]
+#[serde(tag = "oma_uri")]
+pub enum WindowsConfiguration {
+${parts.join("\n")}
+// TODO: Have fallback variant
+}`;
+
+console.log("Done!");
+const outputPath = path.resolve(__dirname, "./windows.rs");
+fs.writeFileSync(outputPath, result);
+
+// const schema = {
+// 	generated_at: new Date().toISOString().substring(0, 10),
+// 	// Intentionally skipped: DevDetail_DDF.xml, DeviceManageabilityDDF.xml, DeviceStatusDDF.xml
+// 	sections: [
+// 		await parseDDF(
+// 			"Policy",
+// 			"Policy settings for Windows devices.",
+// 			fs.readFileSync(path.join(__dirname, "../ddf/PolicyDDF_all.xml"), "utf8"),
+// 		),
+// 		await parseDDF(
+// 			"ActiveSync",
+// 			"TODO",
+// 			fs.readFileSync(
+// 				path.join(__dirname, "../ddf/ActiveSyncCSP_DDF.xml"),
+// 				"utf8",
+// 			),
+// 		),
+// 		await parseDDF(
+// 			"CertificateStore",
+// 			"TODO",
+// 			fs.readFileSync(
+// 				path.join(__dirname, "../ddf/CertificateStore_DDF.xml"),
+// 				"utf8",
+// 			),
+// 		),
+// 		await parseDDF(
+// 			"ClientCertificateInstall",
+// 			"TODO",
+// 			fs.readFileSync(
+// 				path.join(__dirname, "../ddf/ClientCertificateInstall_DDF.xml"),
+// 				"utf8",
+// 			),
+// 		),
+// 		// TODO: CMPolicyEnterprise_DDF
+// 		await parseDDF(
+// 			"Defender",
+// 			"TODO",
+// 			fs.readFileSync(path.join(__dirname, "../ddf/DefenderDDF.xml"), "utf8"),
+// 		),
+// 		// TODO: ...
+// 		await parseDDF(
+// 			"Win32AppInventory",
+// 			"TODO",
+// 			fs.readFileSync(
+// 				path.join(__dirname, "../ddf/Win32AppInventoryDDF.xml"),
+// 				"utf8",
+// 			),
+// 		),
+// 		// await parseDDF(
+// 		// 	"WindowsLicensingDDF",
+// 		// 	"TODO",
+// 		// 	fs.readFileSync(
+// 		// 		path.join(__dirname, "../ddf/WindowsLicensingDDF.XML"),
+// 		// 		"utf8",
+// 		// 	),
+// 		// ),
+// 		// await parseDDF(
+// 		// 	"WindowsSecurityAuditing",
+// 		// 	"TODO",
+// 		// 	fs.readFileSync(
+// 		// 		path.join(__dirname, "../ddf/WindowsSecurityAuditing_DDF.xml"),
+// 		// 		"utf8",
+// 		// 	),
+// 		// ),
+// 	],
+// };
+
+// const schemaPath = path.resolve(__dirname, "../src/windows.rs");
+// const numConfigurations = schema.sections
+// 	.map((s) => s.properties.length)
+// 	.reduce((a, b) => a + b, 0);
+// console.log(
+// 	`Saved schema with ${numConfigurations} configurations to '${schemaPath}'`,
+// );
+// fs.writeFileSync(schemaPath, JSON.stringify(schema, null, 2));
