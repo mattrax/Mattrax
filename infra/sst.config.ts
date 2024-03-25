a9712ef3aa75adb81dcece2bf0b5b579e67704ca
Oscar Beaumont <oscar@otbeaumont.me>
2024-03-25 23:51:15 +0800

    Disable Cloudflare provider for now

--- infra/sst.config.ts
+++ infra/sst.config.ts
@@ -10,9 +10,9 @@ export default $config({
 			removal: input?.stage === "production" ? "retain" : "remove",
 			home: "aws",
 			providers: {
-				cloudflare: {
-					accountId: "f8b7c4b3e3ed4fb3d04a4264e21dd18e",
-				},
+				// cloudflare: {
+				// 	accountId: "f8b7c4b3e3ed4fb3d04a4264e21dd18e",
+				// },
 				aws: {
 					region: "us-west-2",
 				},
@@ -21,18 +21,32 @@ export default $config({
 	},
 	async run() {
 		// Application used for syncing user information from EntraID
-		const application = new azuread.Application("Mattrax", {
-			displayName: "Mattrax",
-			web: {
-				redirectUris: [
-					"https://cloud.mattrax.app/api/ms/link",
-					"https://cloud.mattrax.app/api/enrollment/callback",
-				],
-			},
-		});
+		// WARNING: You must manually setup Publisher verification after deploying this
+		const application = new azuread.Application(
+			"Mattrax",
+			{
+				displayName: "Mattrax",
+				// TODO: Logo
+				// TODO: Home page URL - https://mattrax.app
+				// TODO: Terms of service URL - TODO
+				// TODO: Publisher domain
+				// TODO: Accounts in any organizational directory
 
-		// Used for user uploaded files such as applications
-		const bucket = new sst.aws.Bucket("mattrax-data");
+				web: {
+					redirectUris: [
+						"https://cloud.mattrax.app/api/ms/link",
+						"https://cloud.mattrax.app/api/enrollment/callback",
+					],
+				},
+			},
+			{
+				// After publisher verification we probs don't want this to just disappear
+				retainOnDelete: true,
+			},
+		);
+
+		// // Used for user uploaded files such as applications
+		// const bucket = new sst.aws.Bucket("mattrax-data");
 
 		// TODO: SES role binding
 
