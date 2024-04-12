85e8d0f38b6fdbfac5cf5332cfc05db809375354
Oscar Beaumont <oscar@otbeaumont.me>
2024-04-12 16:39:25 +0800

    Discord > DB for waitlist

--- apps/web/src/api/routes/waitlist.ts
+++ apps/web/src/api/routes/waitlist.ts
@@ -24,22 +24,27 @@ export const waitlistRouter = new Hono<HonoEnv>().post("/", async (c) => {
 		return c.text("Invalid request!");
 	}
 
+	// We do Discord first, incase the DB is down.
+	try {
+		await sendDiscordMessage(
+			[
+				`**name**: ${result.data.name}`,
+				`**interest**: ${result.data.interest}`,
+				`**deployment**: ${result.data.deployment}`,
+				`\`${result.data.email}\``,
+			].join("\n"),
+			env.WAITLIST_DISCORD_WEBHOOK_URL,
+		);
+	} catch (err) {
+		console.error("Failed to send discord message", err);
+	}
+
 	await db.insert(waitlist).values({
 		email: result.data.email,
 		name: result.data.name,
 		interest: result.data.interest,
 		deployment: result.data.deployment,
 	});
-
-	await sendDiscordMessage(
-		[
-			`**name**: ${result.data.name}`,
-			`**interest**: ${result.data.interest}`,
-			`**deployment**: ${result.data.deployment}`,
-			`\`${result.data.email}\``,
-		].join("\n"),
-		env.WAITLIST_DISCORD_WEBHOOK_URL,
-	);
 
 	return c.text("ok");
 });
