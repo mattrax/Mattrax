84e1f3ebcdf6c52f704812a8f0b4291328c513a2
Oscar Beaumont <oscar@otbeaumont.me>
2024-04-18 01:42:10 +0800

    serialization more like gonezation, gottem

--- crates/windows/src/policies.rs
+++ crates/windows/src/policies.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
 use ms_mdm::{SyncBodyChild, SyncML};
+use mx_conflict::{ConflictResolutionStrategy, Deploy, Graph};
 use mx_db::{Db, GetDeviceResult};
 use mx_dmvalue::DmValue;
-use mx_graph::{ConflictResolutionStrategy, Deploy, Graph};
 use mx_policy::{windows::WindowsConfiguration, Configuration};
 
 // TODO: Policies
