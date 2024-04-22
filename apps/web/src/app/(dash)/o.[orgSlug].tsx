ab08960b4df32d73717f3ed20815fb4a5326bff9
Brendan Allan <brendonovich@outlook.com>
2024-04-23 00:36:30 +0800

    use index instead of for in breadcrumbs

--- apps/web/src/app/(dash)/o.[orgSlug].tsx
+++ apps/web/src/app/(dash)/o.[orgSlug].tsx
@@ -3,7 +3,7 @@
 import { As } from "@kobalte/core";
 import { Button } from "@mattrax/ui";
 import { type RouteDefinition, A } from "@solidjs/router";
-import { type ParentProps } from "solid-js";
+import type { ParentProps } from "solid-js";
 import { z } from "zod";
 
 import { AuthContext } from "~/components/AuthContext";
@@ -14,41 +14,41 @@ import { MultiSwitcher } from "./MultiSwitcher";
 import { trpc } from "~/lib";
 
 export function useOrgSlug() {
-	const params = useZodParams({ orgSlug: z.string() });
-	return () => params.orgSlug;
+  const params = useZodParams({ orgSlug: z.string() });
+  return () => params.orgSlug;
 }
 
 export default function Layout(props: ParentProps) {
-	return <>{props.children}</>;
+  return <>{props.children}</>;
 }
 
 const NAV_ITEMS = [
-	{ title: "Overview", href: "" },
-	{ title: "Settings", href: "settings" },
+  { title: "Overview", href: "" },
+  { title: "Settings", href: "settings" },
 ];
 
 export const route = {
-	load: ({ params }) =>
-		trpc.useContext().org.tenants.ensureData({ orgSlug: params.orgSlug! }),
-	info: {
-		NAV_ITEMS,
-		BREADCRUMB: {
-			Component: (props: { href: string }) => {
-				return (
-					<AuthContext>
-						<OrgContext>
-							<div class="flex flex-row items-center py-1 gap-2">
-								<A href={props.href}>{useOrg()().name}</A>
-								<MultiSwitcher>
-									<As component={Button} variant="ghost" size="iconSmall">
-										<IconPhCaretUpDown class="h-5 w-5 -mx-1" />
-									</As>
-								</MultiSwitcher>
-							</div>
-						</OrgContext>
-					</AuthContext>
-				);
-			},
-		},
-	},
+  load: ({ params }) =>
+    trpc.useContext().org.tenants.ensureData({ orgSlug: params.orgSlug! }),
+  info: {
+    NAV_ITEMS,
+    BREADCRUMB: {
+      Component: (props: { href: string }) => {
+        return (
+          <AuthContext>
+            <OrgContext>
+              <div class="flex flex-row items-center py-1 gap-2">
+                <A href={props.href}>{useOrg()().name}</A>
+                <MultiSwitcher>
+                  <As component={Button} variant="ghost" size="iconSmall">
+                    <IconPhCaretUpDown class="h-5 w-5 -mx-1" />
+                  </As>
+                </MultiSwitcher>
+              </div>
+            </OrgContext>
+          </AuthContext>
+        );
+      },
+    },
+  },
 } satisfies RouteDefinition;
--- apps/web/src/components/Breadcrumbs.tsx
+++ apps/web/src/components/Breadcrumbs.tsx
@@ -1,63 +1,66 @@
 /* @refresh skip */
 
-import {
-	type Component,
-	For,
-	type ParentProps,
-	Suspense,
-	createMemo,
-} from "solid-js";
 import { useMatch, useMatches } from "@solidjs/router";
+import {
+  type Component,
+  Index,
+  type ParentProps,
+  Suspense,
+  createMemo,
+} from "solid-js";
+import { Dynamic } from "solid-js/web";
 
 export function Breadcrumbs() {
-	const matches = useMatches();
+  const matches = useMatches();
 
-	const breadcrumbs = createMemo(() =>
-		matches().flatMap((match) => {
-			const Inner:
-				| {
-						Component: Component<{ href: string }>;
-						hasNestedSegments?: boolean;
-				  }
-				| undefined = match.route.info?.BREADCRUMB;
-			return Inner ? [{ ...Inner, match }] : [];
-		}),
-	);
+  const breadcrumbs = createMemo(() =>
+    matches().flatMap((match) => {
+      const Inner:
+        | {
+            Component: Component<{ href: string }>;
+            hasNestedSegments?: boolean;
+          }
+        | undefined = match.route.info?.BREADCRUMB;
+      return Inner ? [{ ...Inner, match }] : [];
+    }),
+  );
 
-	return (
-		<div class="flex flex-row items-center text-sm font-medium space-x-2 text-gray-800">
-			<For each={breadcrumbs()}>
-				{({ Component, hasNestedSegments, match }) => {
-					const _match = useMatch(() => `${match.path}/:segment/:subSegment/*`);
+  return (
+    <div class="flex flex-row items-center text-sm font-medium space-x-2 text-gray-800">
+      <Index each={breadcrumbs()}>
+        {(b) => {
+          const _match = useMatch(
+            () => `${b().match.path}/:segment/:subSegment/*`,
+          );
 
-					const href = createMemo(() => {
-						const __match = hasNestedSegments ? _match() : undefined;
+          const href = createMemo(() => {
+            const __match = b().hasNestedSegments ? _match() : undefined;
 
-						const ret = __match
-							? `${match.path}/${__match.params.segment}`
-							: match.path;
+            const ret = __match
+              ? `${b().match.path}/${__match.params.segment}`
+              : b().match.path;
 
-						return ret;
-					});
+            return ret;
+          });
 
-					return (
-						<Breadcrumb>
-							<Component href={href()} />
-						</Breadcrumb>
-					);
-				}}
-			</For>
-		</div>
-	);
+          return (
+            <Breadcrumb>
+              <Dynamic component={b().Component} href={href()} />
+            </Breadcrumb>
+          );
+        }}
+      </Index>
+    </div>
+  );
 }
 
 export function Breadcrumb(props: ParentProps) {
-	return (
-		<Suspense>
-			<div class="flex flex-row items-center gap-2">
-				<IconMdiSlashForward class="text-lg text-gray-300" />
-				{props.children}
-			</div>
-		</Suspense>
-	);
+  return (
+    <Suspense>
+      <div class="flex flex-row items-center gap-2">
+        <IconMdiSlashForward class="text-lg text-gray-300" />
+        {props.children}
+      </div>
+    </Suspense>
+  );
 }
