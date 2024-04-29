8eb24b46182888ed8f5fba2bd951105df3c15e3a
Brendan Allan <brendonovich@outlook.com>
2024-04-29 22:24:55 +0800

    ingest 2.0

---
+++ apps/ingest/src/main.rs
@@ -0,0 +1,177 @@
+use std::{
+    collections::BTreeMap,
+    fs,
+    path::{Path, PathBuf},
+};
+
+use ms_ddf::{AllowedValueGroupedNodes, DFFormatVariant, MgmtTree, Node};
+use serde::Serialize;
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct WindowsDFFPolicy {
+    name: String,
+    description: Option<String>,
+    #[serde(flatten)]
+    format: Format,
+}
+
+#[derive(Serialize, Debug)]
+#[serde(tag = "format", rename_all = "camelCase")]
+enum Format {
+    Int {
+        default_value: i32,
+        #[serde(flatten)]
+        allowed_values: Option<IntAllowedValues>,
+    },
+    Unknown,
+}
+
+impl Format {
+    fn parse(node: &Node) -> Self {
+        if matches!(*node.properties.df_format, DFFormatVariant::Int) {
+            return Self::Int {
+                default_value: 0,
+                allowed_values: IntAllowedValues::parse(node),
+            };
+        }
+
+        Self::Unknown
+    }
+}
+
+#[derive(Serialize, Debug)]
+#[serde(tag = "valueType", rename_all = "camelCase")]
+enum IntAllowedValues {
+    Range {
+        min: i32,
+        max: i32,
+    },
+    Enum {
+        #[serde(rename = "enum")]
+        r#enum: BTreeMap<String, EnumContent>,
+    },
+}
+
+impl IntAllowedValues {
+    fn parse(node: &Node) -> Option<Self> {
+        let Some(allowed_values) = &node.properties.allowed_values else {
+            return None;
+        };
+
+        Some(match allowed_values.value_type {
+            ms_ddf::ValueType::Range => {
+                let AllowedValueGroupedNodes::Value(value) = &allowed_values.values else {
+                    return None;
+                };
+
+                let Some((min, max)) = value.value[1..value.value.len() - 2].split_once("-") else {
+                    return None;
+                };
+
+                Self::Range {
+                    min: min.parse().ok()?,
+                    max: max.parse().ok()?,
+                }
+            }
+            ms_ddf::ValueType::ENUM => {
+                let AllowedValueGroupedNodes::Enum(variants) = &allowed_values.values else {
+                    return None;
+                };
+
+                Self::Enum {
+                    r#enum: variants
+                        .iter()
+                        .map(|variant| {
+                            (
+                                variant.value.clone(),
+                                EnumContent {
+                                    description: variant.value_description.clone(),
+                                },
+                            )
+                        })
+                        .collect(),
+                }
+            }
+            _ => {
+                return None;
+            }
+        })
+    }
+}
+
+#[derive(Serialize, Debug)]
+struct EnumContent {
+    description: Option<String>,
+}
+
+type WindowsDFFPolicyCollection = BTreeMap<PathBuf, WindowsDFFPolicy>;
+
+fn handle_node(node: &Node, path: &PathBuf) -> WindowsDFFPolicyCollection {
+    let mut collection = WindowsDFFPolicyCollection::default();
+
+    let path = node
+        .path
+        .as_ref()
+        .map(|new_path| path.join(new_path))
+        .unwrap_or_else(|| path.clone())
+        .join(&node.node_name);
+
+    if !node.children.is_empty() {
+        for child in &node.children {
+            collection.extend(handle_node(child, &path));
+        }
+    }
+
+    let access_type = &node.properties.access_type;
+    if access_type.len() == 1 && (access_type.add.is_some() || access_type.exec.is_some()) {
+        return collection;
+    }
+
+    dbg!(&path);
+    collection.insert(
+        path,
+        WindowsDFFPolicy {
+            name: node.node_name.clone(),
+            description: node.properties.description.clone(),
+            format: Format::parse(&node),
+        },
+    );
+
+    collection
+}
+
+fn handle_mgmt_tree(tree: MgmtTree) -> WindowsDFFPolicyCollection {
+    let mut policies = WindowsDFFPolicyCollection::new();
+
+    for node in tree.nodes {
+        policies.extend(handle_node(&node, &PathBuf::new()))
+    }
+
+    policies
+}
+
+fn main() {
+    let files = fs::read_dir(Path::new(env!("CARGO_MANIFEST_DIR")).join("./ddf")).unwrap();
+
+    let mut policy_collection = WindowsDFFPolicyCollection::new();
+
+    for file in files {
+        let file = file.unwrap();
+        if !file.file_name().to_string_lossy().ends_with(".xml") {
+            continue;
+        }
+        let contents = fs::read(file.path()).unwrap();
+
+        let root: MgmtTree = easy_xml::de::from_bytes(contents.as_slice())
+            .expect(&format!("Failed to parse {:?}", file.path()));
+
+        policy_collection.extend(handle_mgmt_tree(root));
+    }
+
+    fs::write(
+        Path::new(env!("CARGO_MANIFEST_DIR")).join("out.json"),
+        serde_json::to_string_pretty(&policy_collection).unwrap(),
+    )
+    .unwrap();
+}
---
+++ crates/ms-ddf/src/ddf_v2.rs
@@ -0,0 +1,466 @@
+use easy_xml_derive::XmlDeserialize;
+
+use crate::msft;
+
+#[derive(XmlDeserialize, Debug)]
+#[easy_xml(root)]
+pub struct MgmtTree {
+    #[easy_xml(rename = "VerDTD")]
+    pub ver_dtd: String,
+    #[easy_xml(rename = "Node")]
+    pub nodes: Vec<Node>,
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub struct Node {
+    #[easy_xml(rename = "NodeName")]
+    pub node_name: String,
+    #[easy_xml(rename = "Path")]
+    pub path: Option<String>,
+    #[easy_xml(rename = "Node")]
+    pub children: Vec<Node>,
+    #[easy_xml(rename = "DFProperties")]
+    pub properties: DFProperties,
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub struct DFProperties {
+    #[easy_xml(rename = "AccessType")]
+    pub access_type: AccessType,
+    #[easy_xml(rename = "DefaultValue")]
+    pub default_value: Option<String>,
+    #[easy_xml(rename = "Description")]
+    pub description: Option<String>,
+    #[easy_xml(rename = "DFFormat")]
+    pub df_format: DFFormat,
+    #[easy_xml(rename = "Occurrence")]
+    pub occurrence: Option<Occurrence>,
+    #[easy_xml(rename = "Scope")]
+    pub scope: Option<Scope>,
+    #[easy_xml(rename = "DFTitle")]
+    pub df_title: Option<String>,
+    #[easy_xml(rename = "DFType")]
+    pub df_type: String,
+    #[easy_xml(rename = "CaseSense")]
+    pub case_sense: Option<String>,
+    #[easy_xml(rename = "AllowedValues")]
+    pub allowed_values: Option<msft::AllowedValues>,
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub struct AccessType {
+    #[easy_xml(rename = "Add")]
+    pub add: Option<Element>,
+    #[easy_xml(rename = "Copy")]
+    pub copy: Option<Element>,
+    #[easy_xml(rename = "Delete")]
+    pub delete: Option<Element>,
+    #[easy_xml(rename = "Exec")]
+    pub exec: Option<Element>,
+    #[easy_xml(rename = "Get")]
+    pub get: Option<Element>,
+    #[easy_xml(rename = "Replace")]
+    pub replace: Option<Element>,
+}
+
+impl AccessType {
+    pub fn len(&self) -> u8 {
+        let mut count = 0;
+        if self.add.is_some() {
+            count += 1;
+        }
+        if self.copy.is_some() {
+            count += 1;
+        }
+        if self.delete.is_some() {
+            count += 1;
+        }
+        if self.exec.is_some() {
+            count += 1;
+        }
+        if self.get.is_some() {
+            count += 1;
+        }
+        if self.replace.is_some() {
+            count += 1;
+        }
+        count
+    }
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub struct DFFormat {
+    #[easy_xml(rename = "b64|bin|bool|chr|int|node|null|xml|date|time|float")]
+    variant: DFFormatVariant,
+}
+
+impl std::ops::Deref for DFFormat {
+    type Target = DFFormatVariant;
+
+    fn deref(&self) -> &Self::Target {
+        &self.variant
+    }
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub enum DFFormatVariant {
+    #[easy_xml(rename = "b64")]
+    Base64,
+    #[easy_xml(rename = "bin")]
+    Bin,
+    #[easy_xml(rename = "bool")]
+    Bool,
+    #[easy_xml(rename = "chr")]
+    String,
+    #[easy_xml(rename = "int")]
+    Int,
+    #[easy_xml(rename = "node")]
+    Node,
+    #[easy_xml(rename = "null")]
+    Null,
+    #[easy_xml(rename = "xml")]
+    Xml,
+    #[easy_xml(rename = "date")]
+    Date,
+    #[easy_xml(rename = "time")]
+    Time,
+    #[easy_xml(rename = "float")]
+    Float,
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub struct Occurrence {
+    #[easy_xml(rename = "One|ZeroOrOne|ZeroOrMore|OneOrMore|ZeroOrN|OneOrN")]
+    variant: OccurrenceVariant,
+}
+
+impl std::ops::Deref for Occurrence {
+    type Target = OccurrenceVariant;
+
+    fn deref(&self) -> &Self::Target {
+        &self.variant
+    }
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub enum OccurrenceVariant {
+    One,
+    ZeroOrOne,
+    ZeroOrMore,
+    OneOrMore,
+    ZeroOrN(#[easy_xml(text)] i32),
+    OneOrN(#[easy_xml(text)] i32),
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub struct Scope {
+    #[easy_xml(rename = "Permanent|Dynamic")]
+    variant: ScopeVariant,
+}
+
+impl std::ops::Deref for Scope {
+    type Target = ScopeVariant;
+
+    fn deref(&self) -> &Self::Target {
+        &self.variant
+    }
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub enum ScopeVariant {
+    Permanent,
+    Dynamic,
+}
+
+#[derive(XmlDeserialize, Debug)]
+pub struct Element;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_test() {
+        let doc = easy_xml::de::from_str::<MgmtTree>(Education_AreaDDF).unwrap();
+        dbg!(doc);
+    }
+
+    const Education_AreaDDF: &str = r#"<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE MgmtTree PUBLIC " -//OMA//DTD-DM-DDF 1.2//EN"
+  "http://www.openmobilealliance.org/tech/DTD/DM_DDF-V1_2.dtd"
+  [<?oma-dm-ddf-ver supported-versions="1.2"?>]>
+<MgmtTree xmlns:MSFT="http://schemas.microsoft.com/MobileDevice/DM">
+  <VerDTD>1.2</VerDTD>
+  <Node>
+    <NodeName>Education</NodeName>
+    <Path>./User/Vendor/MSFT/Policy/Config</Path>
+    <DFProperties>
+      <AccessType>
+        <Get />
+      </AccessType>
+      <DFFormat>
+        <node />
+      </DFFormat>
+      <Occurrence>
+        <One />
+      </Occurrence>
+      <Scope>
+        <Permanent />
+      </Scope>
+      <DFType>
+        <MIME>com.microsoft/11/MDM/Policy</MIME>
+      </DFType>
+    </DFProperties>
+    <Node>
+      <NodeName>AllowGraphingCalculator</NodeName>
+      <DFProperties>
+        <AccessType>
+          <Add />
+          <Delete />
+          <Get />
+          <Replace />
+        </AccessType>
+        <Description>This policy setting allows you to control whether graphing functionality is available in the Windows Calculator app. If you disable this policy setting, graphing functionality will not be accessible in the Windows Calculator app. If you enable or don't configure this policy setting, users will be able to access graphing functionality.</Description>
+        <DFFormat>
+          <int />
+        </DFFormat>
+        <Occurrence>
+          <ZeroOrOne />
+        </Occurrence>
+        <Scope>
+          <Dynamic />
+        </Scope>
+        <DFType>
+          <MIME>text/plain</MIME>
+        </DFType>
+        <DefaultValue>1</DefaultValue>
+        <MSFT:Applicability>
+          <MSFT:OsBuildVersion>10.0.19041</MSFT:OsBuildVersion>
+          <MSFT:CspVersion>10.0</MSFT:CspVersion>
+          <MSFT:EditionAllowList>0x4;0x1B;0x30;0x31;0x48;0x54;0x62;0x63;0x64;0x65;0x79;0x7A;0x7D;0x7E;0x81;0x82;0x8A;0x8B;0xA1;0xA2;0xA4;0xA5;0xAB;0xAC;0xBC;0xBF;0xCA;0xCB;0xCD;</MSFT:EditionAllowList>
+        </MSFT:Applicability>
+        <MSFT:AllowedValues ValueType="ENUM">
+          <MSFT:Enum>
+            <MSFT:Value>0</MSFT:Value>
+            <MSFT:ValueDescription>Disabled.</MSFT:ValueDescription>
+          </MSFT:Enum>
+          <MSFT:Enum>
+            <MSFT:Value>1</MSFT:Value>
+            <MSFT:ValueDescription>Enabled.</MSFT:ValueDescription>
+          </MSFT:Enum>
+        </MSFT:AllowedValues>
+        <MSFT:GpMapping GpEnglishName="AllowGraphingCalculator" GpAreaPath="Programs~AT~WindowsComponents~Calculator" />
+        <MSFT:ConflictResolution>LowestValueMostSecure</MSFT:ConflictResolution>
+      </DFProperties>
+    </Node>
+    <Node>
+      <NodeName>DefaultPrinterName</NodeName>
+      <DFProperties>
+        <AccessType>
+          <Add />
+          <Delete />
+          <Get />
+          <Replace />
+        </AccessType>
+        <Description>This policy sets user's default printer</Description>
+        <DFFormat>
+          <chr />
+        </DFFormat>
+        <Occurrence>
+          <ZeroOrOne />
+        </Occurrence>
+        <Scope>
+          <Dynamic />
+        </Scope>
+        <DFType>
+          <MIME>text/plain</MIME>
+        </DFType>
+        <MSFT:Applicability>
+          <MSFT:OsBuildVersion>10.0.16299</MSFT:OsBuildVersion>
+          <MSFT:CspVersion>6.0</MSFT:CspVersion>
+          <MSFT:EditionAllowList>0x4;0x1B;0x30;0x31;0x48;0x54;0x79;0x7A;0x7D;0x7E;0x81;0x82;0x8A;0x8B;0xA1;0xA2;0xA4;0xA5;0xAB;0xAC;0xBC;0xBF;0xCA;0xCB;0xCD;</MSFT:EditionAllowList>
+        </MSFT:Applicability>
+        <MSFT:ConflictResolution>LastWrite</MSFT:ConflictResolution>
+      </DFProperties>
+    </Node>
+    <Node>
+      <NodeName>PreventAddingNewPrinters</NodeName>
+      <DFProperties>
+        <AccessType>
+          <Add />
+          <Delete />
+          <Get />
+          <Replace />
+        </AccessType>
+        <Description>Boolean that specifies whether or not to prevent user to install new printers</Description>
+        <DFFormat>
+          <int />
+        </DFFormat>
+        <Occurrence>
+          <ZeroOrOne />
+        </Occurrence>
+        <Scope>
+          <Dynamic />
+        </Scope>
+        <DFType>
+          <MIME>text/plain</MIME>
+        </DFType>
+        <DefaultValue>0</DefaultValue>
+        <MSFT:Applicability>
+          <MSFT:OsBuildVersion>10.0.16299</MSFT:OsBuildVersion>
+          <MSFT:CspVersion>6.0</MSFT:CspVersion>
+          <MSFT:EditionAllowList>0x4;0x1B;0x30;0x31;0x48;0x54;0x79;0x7A;0x7D;0x7E;0x81;0x82;0x8A;0x8B;0xA1;0xA2;0xA4;0xA5;0xAB;0xAC;0xBC;0xBF;0xCA;0xCB;0xCD;</MSFT:EditionAllowList>
+        </MSFT:Applicability>
+        <MSFT:AllowedValues ValueType="ENUM">
+          <MSFT:Enum>
+            <MSFT:Value>0</MSFT:Value>
+            <MSFT:ValueDescription>Allow user installation.</MSFT:ValueDescription>
+          </MSFT:Enum>
+          <MSFT:Enum>
+            <MSFT:Value>1</MSFT:Value>
+            <MSFT:ValueDescription>Prevent user installation.</MSFT:ValueDescription>
+          </MSFT:Enum>
+        </MSFT:AllowedValues>
+        <MSFT:GpMapping GpEnglishName="NoAddPrinter" GpAreaPath="Printing~AT~ControlPanel~CplPrinters" />
+        <MSFT:ConflictResolution>HighestValueMostSecure</MSFT:ConflictResolution>
+      </DFProperties>
+    </Node>
+    <Node>
+      <NodeName>PrinterNames</NodeName>
+      <DFProperties>
+        <AccessType>
+          <Add />
+          <Delete />
+          <Get />
+          <Replace />
+        </AccessType>
+        <Description>This policy provisions per-user network printers</Description>
+        <DFFormat>
+          <chr />
+        </DFFormat>
+        <Occurrence>
+          <ZeroOrOne />
+        </Occurrence>
+        <Scope>
+          <Dynamic />
+        </Scope>
+        <DFType>
+          <MIME>text/plain</MIME>
+        </DFType>
+        <MSFT:Applicability>
+          <MSFT:OsBuildVersion>10.0.16299</MSFT:OsBuildVersion>
+          <MSFT:CspVersion>6.0</MSFT:CspVersion>
+          <MSFT:EditionAllowList>0x4;0x1B;0x30;0x31;0x48;0x54;0x79;0x7A;0x7D;0x7E;0x81;0x82;0x8A;0x8B;0xA1;0xA2;0xA4;0xA5;0xAB;0xAC;0xBC;0xBF;0xCA;0xCB;0xCD;</MSFT:EditionAllowList>
+        </MSFT:Applicability>
+        <MSFT:AllowedValues ValueType="None">
+          <MSFT:List Delimiter="0xF000" />
+        </MSFT:AllowedValues>
+        <MSFT:ConflictResolution>LastWrite</MSFT:ConflictResolution>
+      </DFProperties>
+    </Node>
+  </Node>
+  <Node>
+    <NodeName>Education</NodeName>
+    <Path>./Device/Vendor/MSFT/Policy/Config</Path>
+    <DFProperties>
+      <AccessType>
+        <Get />
+      </AccessType>
+      <DFFormat>
+        <node />
+      </DFFormat>
+      <Occurrence>
+        <One />
+      </Occurrence>
+      <Scope>
+        <Permanent />
+      </Scope>
+      <DFType>
+        <MIME>com.microsoft/11/MDM/Policy</MIME>
+      </DFType>
+    </DFProperties>
+    <Node>
+      <NodeName>EnableEduThemes</NodeName>
+      <DFProperties>
+        <AccessType>
+          <Add />
+          <Delete />
+          <Get />
+          <Replace />
+        </AccessType>
+        <Description>This policy setting allows you to control whether EDU-specific theme packs are available in Settings &gt; Personalization. If you disable or don't configure this policy setting, EDU-specific theme packs will not be included. If you enable this policy setting, users will be able to personalize their devices with EDU-specific themes.</Description>
+        <DFFormat>
+          <int />
+        </DFFormat>
+        <Occurrence>
+          <ZeroOrOne />
+        </Occurrence>
+        <Scope>
+          <Dynamic />
+        </Scope>
+        <DFType>
+          <MIME>text/plain</MIME>
+        </DFType>
+        <DefaultValue>0</DefaultValue>
+        <MSFT:Applicability>
+          <MSFT:OsBuildVersion>10.0.22621</MSFT:OsBuildVersion>
+          <MSFT:CspVersion>11.0</MSFT:CspVersion>
+          <MSFT:EditionAllowList>0x4;0x1B;0x30;0x31;0x48;0x54;0x79;0x7A;0x7D;0x7E;0x81;0x82;0x8A;0x8B;0xA1;0xA2;0xA4;0xA5;0xAB;0xAC;0xBC;0xBF;0xCA;0xCB;0xCD;</MSFT:EditionAllowList>
+        </MSFT:Applicability>
+        <MSFT:AllowedValues ValueType="ENUM">
+          <MSFT:Enum>
+            <MSFT:Value>0</MSFT:Value>
+            <MSFT:ValueDescription>Disabled.</MSFT:ValueDescription>
+          </MSFT:Enum>
+          <MSFT:Enum>
+            <MSFT:Value>1</MSFT:Value>
+            <MSFT:ValueDescription>Enabled.</MSFT:ValueDescription>
+          </MSFT:Enum>
+        </MSFT:AllowedValues>
+        <MSFT:ConflictResolution>LowestValueMostSecure</MSFT:ConflictResolution>
+      </DFProperties>
+    </Node>
+    <Node>
+      <NodeName>IsEducationEnvironment</NodeName>
+      <DFProperties>
+        <AccessType>
+          <Add />
+          <Delete />
+          <Get />
+          <Replace />
+        </AccessType>
+        <Description>This policy setting allows tenant to control whether to declare this OS as an education environment</Description>
+        <DFFormat>
+          <int />
+        </DFFormat>
+        <Occurrence>
+          <ZeroOrOne />
+        </Occurrence>
+        <Scope>
+          <Dynamic />
+        </Scope>
+        <DFType>
+          <MIME>text/plain</MIME>
+        </DFType>
+        <DefaultValue>0</DefaultValue>
+        <MSFT:Applicability>
+          <MSFT:OsBuildVersion>10.0.22621</MSFT:OsBuildVersion>
+          <MSFT:CspVersion>11.0</MSFT:CspVersion>
+          <MSFT:EditionAllowList>0x4;0x1B;0x30;0x31;0x48;0x54;0x79;0x7A;0x7D;0x7E;0x81;0x82;0x8A;0x8B;0xA1;0xA2;0xA4;0xA5;0xAB;0xAC;0xBC;0xBF;0xCA;0xCB;0xCD;</MSFT:EditionAllowList>
+        </MSFT:Applicability>
+        <MSFT:AllowedValues ValueType="ENUM">
+          <MSFT:Enum>
+            <MSFT:Value>0</MSFT:Value>
+            <MSFT:ValueDescription>Disabled.</MSFT:ValueDescription>
+          </MSFT:Enum>
+          <MSFT:Enum>
+            <MSFT:Value>1</MSFT:Value>
+            <MSFT:ValueDescription>Enabled.</MSFT:ValueDescription>
+          </MSFT:Enum>
+        </MSFT:AllowedValues>
+        <MSFT:ConflictResolution>LowestValueMostSecure</MSFT:ConflictResolution>
+      </DFProperties>
+    </Node>
+  </Node>
+</MgmtTree>"#;
+}
