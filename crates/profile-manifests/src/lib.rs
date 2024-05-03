fe4c4878bc7d9f67c9fe2fde15ebb564d9685ae3
Brendan Allan <brendonovich@outlook.com>
2024-05-03 11:41:23 +0800

    delete profile-manifests

--- crates/profile-manifests/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Parser for the [Profile Manifests](https://github.com/ProfileCreator/ProfileManifests) format.
-
-mod manifest;
-mod parse_from_dir;
-mod validate;
-
-pub use manifest::*;
-pub use parse_from_dir::*;
-pub use validate::*;
