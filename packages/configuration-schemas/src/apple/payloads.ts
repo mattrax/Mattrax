91c2d2a719b4a0b8ea3a640fcbbcc14a22e6d26c
Brendan Allan <brendonovich@outlook.com>
2024-05-16 15:07:58 +0800

    formatting

--- packages/configuration-schemas/src/apple/payloads.ts
+++ packages/configuration-schemas/src/apple/payloads.ts
@@ -1,3 +1,25 @@
-export type AppleProfilePayload = { title: string; description: string; properties: { [key in string]: Property }; supervised: boolean }
-export type Property = { title?: string | null; description?: string | null; type: PropertyType }
-export type PropertyType = { array: PropertyType[] } | "boolean" | "date" | "data" | { dictionary: { [key in string]: Property } } | "integer" | "real" | "float" | "string" | "url" | "alias" | "unionPolicy" | "plist"
+export type AppleProfilePayload = {
+	title: string;
+	description: string;
+	properties: { [key in string]: Property };
+	supervised: boolean;
+};
+export type Property = {
+	title?: string | null;
+	description?: string | null;
+	type: PropertyType;
+};
+export type PropertyType =
+	| { array: PropertyType[] }
+	| "boolean"
+	| "date"
+	| "data"
+	| { dictionary: { [key in string]: Property } }
+	| "integer"
+	| "real"
+	| "float"
+	| "string"
+	| "url"
+	| "alias"
+	| "unionPolicy"
+	| "plist";
