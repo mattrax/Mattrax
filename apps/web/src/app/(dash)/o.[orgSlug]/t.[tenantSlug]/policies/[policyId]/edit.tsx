3ea03cabb6b89b88bef3bf9aa6b325ed1938d734
Brendan Allan <brendonovich@outlook.com>
2024-05-16 12:38:50 +0800

    lazy load configuration schemas

--- apps/web/src/app/(dash)/o.[orgSlug]/t.[tenantSlug]/policies/[policyId]/edit.tsx
+++ apps/web/src/app/(dash)/o.[orgSlug]/t.[tenantSlug]/policies/[policyId]/edit.tsx
@@ -1,5 +1,3 @@
-import windowsPolicies from "@mattrax/configuration-schemas/windows/ddf.json";
-import applePayloads from "@mattrax/configuration-schemas/apple/payloads.json";
 import { createContentEditableController } from "@mattrax/ui/lib";
 import {
 	PolicyComposer,
@@ -10,6 +8,7 @@ import { PageLayout, PageLayoutHeading } from "~c/PageLayout";
 import { PolicyContext, usePolicy } from "./Context";
 import { createSignal, Show } from "solid-js";
 import { useFeatures } from "~/lib/featureFlags";
+import { createAsync } from "@solidjs/router";
 
 export default function Page() {
 	const policy = () => usePolicy()();
@@ -17,13 +16,26 @@ export default function Page() {
 	const policyNameController = createContentEditableController(setPolicyName);
 	const controller = createPolicyComposerController();
 
+	const windowsPolicies = createAsync(() =>
+		import("@mattrax/configuration-schemas/windows/ddf.json").then(
+			(i) => i.default,
+		),
+	);
+	const applePayloads = createAsync(() =>
+		import("@mattrax/configuration-schemas/apple/payloads.json").then(
+			(i) => i.default,
+		),
+	);
+
 	return (
 		<PolicyContext>
-			<PolicyComposer
-				windowsCSPs={windowsPolicies as any}
-				applePayloads={applePayloads as any}
-				controller={controller}
-			/>
+			<Show when={windowsPolicies() && applePayloads()}>
+				<PolicyComposer
+					windowsCSPs={windowsPolicies() as any}
+					applePayloads={applePayloads() as any}
+					controller={controller}
+				/>
+			</Show>
 		</PolicyContext>
 	);
 }
