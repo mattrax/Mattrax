fb7efefb6f655a4fc65f367006069e09ceb10c7f
Brendan Allan <brendonovich@outlook.com>
2024-05-17 16:41:06 +0800

    pulumi via ion

--- apps/web/src/db/index.ts
+++ apps/web/src/db/index.ts
@@ -7,12 +7,12 @@ export * from "./schema";
 import * as schema from "./schema";
 
 const client = new Client({
-	url: env.DATABASE_URL,
-	// Cloudflare Worker's doesn't like `cache`
-	fetch: (url, init) => {
-		(init as any).cache = undefined;
-		return fetch(url, init);
-	},
+  url: env.PLANETSCALE_URL,
+  // Cloudflare Worker's doesn't like `cache`
+  fetch: (url, init) => {
+    (init as any).cache = undefined;
+    return fetch(url, init);
+  },
 });
 
 export const db = drizzle(client, { schema, logger: false });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,18 +2,18 @@ import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 dotenv.config({
-	path: "../.env",
+  path: "../.env",
 });
 
-if ("DATABASE_URL" in process.env === false)
-	throw new Error("'DATABASE_URL' not set in env");
+if ("PLANETSCALE_URL" in process.env === false)
+  throw new Error("'PLANETSCALE_URL' not set in env");
 
 export default defineConfig({
-	schema: "./apps/web/src/db/schema.ts",
-	driver: "mysql2",
-	dbCredentials: {
-		uri: process.env.DATABASE_URL!,
-	},
-	verbose: true,
-	strict: true,
+  schema: "./apps/web/src/db/schema.ts",
+  driver: "mysql2",
+  dbCredentials: {
+    uri: process.env.PLANETSCALE_URL!,
+  },
+  verbose: true,
+  strict: true,
 });
