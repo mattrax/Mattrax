d819eb028b55e9c4ec807eb337e5e5cfa1884efc
Oscar Beaumont <oscar@otbeaumont.me>
2024-05-18 14:07:12 +0800

    test

--- apps/web/src/entry-server.tsx
+++ apps/web/src/entry-server.tsx
@@ -1,26 +1,32 @@
 // @refresh reload
 import { createHandler, StartServer } from "@solidjs/start/server";
+import { getRequestEvent } from "solid-js/web";
 
-export default createHandler(() => (
-	<StartServer
-		document={({ assets, children, scripts }) => (
-			<html lang="en" class="h-full">
-				<head>
-					<meta charset="UTF-8" />
-					{/* <link rel="icon" type="image/svg+xml" href="/vite.svg" /> */}
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1.0"
-					/>
-					{assets}
-				</head>
-				<body class="h-full">
-					<div id="app" class="flex min-h-full flex-col">
-						{children}
-					</div>
-					{scripts}
-				</body>
-			</html>
-		)}
-	/>
-));
+export default createHandler(() => {
+	// console.log(getRequestEvent());
+
+	// return (
+	// 	<StartServer
+	// 		document={({ assets, children, scripts }) => (
+	// 			<html lang="en" class="h-full">
+	// 				<head>
+	// 					<meta charset="UTF-8" />
+	// 					{/* <link rel="icon" type="image/svg+xml" href="/vite.svg" /> */}
+	// 					<meta
+	// 						name="viewport"
+	// 						content="width=device-width, initial-scale=1.0"
+	// 					/>
+	// 					{assets}
+	// 				</head>
+	// 				<body class="h-full">
+	// 					<div id="app" class="flex min-h-full flex-col">
+	// 						{children}
+	// 					</div>
+	// 					{scripts}
+	// 				</body>
+	// 			</html>
+	// 		)}
+	// 	/>
+	// );
+	return <h1>Hello Wortld</h1>;
+});
