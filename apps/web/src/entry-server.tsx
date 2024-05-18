e51a640421ab0bffa8fb24237ecb409dc7208bbe
Oscar Beaumont <oscar@otbeaumont.me>
2024-05-18 14:20:25 +0800

    a

--- apps/web/src/entry-server.tsx
+++ apps/web/src/entry-server.tsx
@@ -1,32 +1,41 @@
 // @refresh reload
-import { createHandler, StartServer } from "@solidjs/start/server";
-import { getRequestEvent } from "solid-js/web";
+// import { createHandler, StartServer } from "@solidjs/start/server";
+// import { getRequestEvent } from "solid-js/web";
 
-export default createHandler(() => {
-	// console.log(getRequestEvent());
+// export default createHandler(() => {
+// 	// console.log(getRequestEvent());
 
-	// return (
-	// 	<StartServer
-	// 		document={({ assets, children, scripts }) => (
-	// 			<html lang="en" class="h-full">
-	// 				<head>
-	// 					<meta charset="UTF-8" />
-	// 					{/* <link rel="icon" type="image/svg+xml" href="/vite.svg" /> */}
-	// 					<meta
-	// 						name="viewport"
-	// 						content="width=device-width, initial-scale=1.0"
-	// 					/>
-	// 					{assets}
-	// 				</head>
-	// 				<body class="h-full">
-	// 					<div id="app" class="flex min-h-full flex-col">
-	// 						{children}
-	// 					</div>
-	// 					{scripts}
-	// 				</body>
-	// 			</html>
-	// 		)}
-	// 	/>
-	// );
-	return <h1>Hello Wortld</h1>;
+// 	// return (
+// 	// 	<StartServer
+// 	// 		document={({ assets, children, scripts }) => (
+// 	// 			<html lang="en" class="h-full">
+// 	// 				<head>
+// 	// 					<meta charset="UTF-8" />
+// 	// 					{/* <link rel="icon" type="image/svg+xml" href="/vite.svg" /> */}
+// 	// 					<meta
+// 	// 						name="viewport"
+// 	// 						content="width=device-width, initial-scale=1.0"
+// 	// 					/>
+// 	// 					{assets}
+// 	// 				</head>
+// 	// 				<body class="h-full">
+// 	// 					<div id="app" class="flex min-h-full flex-col">
+// 	// 						{children}
+// 	// 					</div>
+// 	// 					{scripts}
+// 	// 				</body>
+// 	// 			</html>
+// 	// 		)}
+// 	// 	/>
+// 	// );
+// 	return <h1>Hello Wortld</h1>;
+
+// });
+
+import { type HTTPEvent, eventHandler } from "vinxi/http";
+
+eventHandler({
+	handler: (e: HTTPEvent) => {
+		return new Response("hello world");
+	},
 });
