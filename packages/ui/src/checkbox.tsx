fb448df36450823ac74db825a164b85c8c8253a0
Brendan Allan <brendonovich@outlook.com>
2024-05-24 01:59:43 +0800

    add position: relative to checkbox input

--- packages/ui/src/checkbox.tsx
+++ packages/ui/src/checkbox.tsx
@@ -14,7 +14,7 @@ const Checkbox = <T extends ValidComponent = "div">(
 	const [, rest] = splitProps(props as any, ["class"]);
 	return (
 		<CheckboxPrimitive.Root class={cn("items-top flex", props.class)} {...rest}>
-			<CheckboxPrimitive.Input class="peer" />
+			<CheckboxPrimitive.Input class="peer" style={{ position: "relative" }} />
 			<CheckboxPrimitive.Control
 				class="border-primary ring-offset-background data-[checked]:bg-primary data-[checked]:text-primary-foreground h-4 w-4 shrink-0 rounded-sm border peer-focus-visible:outline-none peer-focus-visible:ring-2 peer-focus-visible:ring-offset-2 peer-focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[checked]:border-none"
 				onClick={(e) => {
