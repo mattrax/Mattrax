01d5786bede116b8ea011195d8e87c255670e4f6
Brendan Allan <brendonovich@outlook.com>
2024-05-24 01:18:37 +0800

    fix breadcrumb suspense

--- apps/web/src/app/(dash)/@topbar/@breadcrumbs.tsx
+++ apps/web/src/app/(dash)/@topbar/@breadcrumbs.tsx
@@ -1,36 +1,22 @@
-import {
-	For,
-	type ParentProps,
-	Suspense,
-	SuspenseList,
-	children,
-} from "solid-js";
-
-import IconMdiSlashForward from "~icons/mdi/slash-forward";
+import { Show, For, type ParentProps, SuspenseList, children } from "solid-js";
 
 export default function (props: ParentProps) {
-	const breadcrumbs = children(() => props.children);
-
 	return (
 		<div class="flex flex-row items-center text-sm font-medium space-x-2 text-gray-800">
 			<SuspenseList revealOrder="forwards" tail="collapsed">
-				<For each={breadcrumbs.toArray()}>
-					{(element: any) => (
-						<div class="flex flex-row items-center gap-2">
-							<Suspense
-								fallback={
-									<>
-										<IconMdiSlashForward class="text-lg text-gray-300" />
-										<div class="w-24 h-4 rounded-full bg-neutral-200 animate-pulse" />
-									</>
-								}
-							>
-								<IconMdiSlashForward class="text-lg text-gray-300" />
-								{element?.breadcrumb}
-							</Suspense>
-						</div>
-					)}
-				</For>
+				<Show when>
+					{(_) => {
+						// children() has a createMemo that will invoke suspense before rendering
+						// so we need to put it here
+						const breadcrumbs = children(() => props.children);
+
+						return (
+							<For each={breadcrumbs.toArray()}>
+								{(element: any) => <>{element?.breadcrumb}</>}
+							</For>
+						);
+					}}
+				</Show>
 			</SuspenseList>
 		</div>
 	);
--- apps/web/src/app/(dash)/@topbar/@breadcrumbs/Breadcrumb.tsx
+++ apps/web/src/app/(dash)/@topbar/@breadcrumbs/Breadcrumb.tsx
@@ -1,6 +1,23 @@
-import type { JSX, ParentProps } from "solid-js";
+import { Suspense, type JSX, type ParentProps } from "solid-js";
 
 // we use a dedicated object since otherwise all the fragments would merge
 export function Breadcrumb(props: ParentProps) {
-	return { breadcrumb: <>{props.children}</> } as unknown as JSX.Element;
+	return {
+		breadcrumb: (
+			<div class="flex flex-row items-center gap-2">
+				<Suspense
+					fallback={
+						<>
+							<IconMdiSlashForward class="text-lg text-gray-300" />
+							<div class="w-24 h-4 rounded-full bg-neutral-200 animate-pulse" />
+						</>
+					}
+				>
+					<IconMdiSlashForward class="text-lg text-gray-300" />
+
+					{props.children}
+				</Suspense>
+			</div>
+		),
+	} as unknown as JSX.Element;
 }
