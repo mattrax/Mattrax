97a44fe043c52a84b42dd9fc21922c84842d81d0
Brendan Allan <brendonovich@outlook.com>
2024-06-01 20:15:21 +0800

    use cloudflare api token

--- infra/sst-env.d.ts
+++ infra/sst-env.d.ts
@@ -1,41 +1,41 @@
 /* tslint:disable */
 /* eslint-disable */
-import "sst";
+import "sst"
 declare module "sst" {
-	export interface Resource {
-		DatabaseURL: {
-			type: "sst.sst.Secret";
-			value: string;
-		};
-		FeedbackDiscordWebhookURL: {
-			type: "sst.sst.Secret";
-			value: string;
-		};
-		InternalSecret: {
-			type: "sst.sst.Secret";
-			value: string;
-		};
-		MattraxEntraIDApplication: {
-			cilentId: string;
-			type: "azuread.index/application.Application";
-		};
-		MattraxEntraIDApplicationPassword: {
-			type: "azuread.index/applicationPassword.ApplicationPassword";
-			value: string;
-		};
-		MattraxWebIAMUserAccessKey: {
-			id: string;
-			secret: string;
-			type: "aws.iam/accessKey.AccessKey";
-		};
-		StripeSecretKey: {
-			type: "sst.sst.Secret";
-			value: string;
-		};
-		WaitlistDiscordWebhookURL: {
-			type: "sst.sst.Secret";
-			value: string;
-		};
-	}
+  export interface Resource {
+    DatabaseURL: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    FeedbackDiscordWebhookURL: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    InternalSecret: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    MattraxEntraIDApplication: {
+      cilentId: string
+      type: "azuread.index/application.Application"
+    }
+    MattraxEntraIDApplicationPassword: {
+      type: "azuread.index/applicationPassword.ApplicationPassword"
+      value: string
+    }
+    MattraxWebIAMUserAccessKey: {
+      id: string
+      secret: string
+      type: "aws.iam/accessKey.AccessKey"
+    }
+    StripeSecretKey: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    WaitlistDiscordWebhookURL: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+  }
 }
-export {};
+export {}
