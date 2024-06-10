86258383b7332a429e2fe275b169e9653e17452f
Oscar Beaumont <oscar@otbeaumont.me>
2024-06-10 20:47:23 +0800

    fixed?

--- apps/web/src/app/(dash)/o.[orgSlug]/t.[tenantSlug]/devices/(devices).tsx
+++ apps/web/src/app/(dash)/o.[orgSlug]/t.[tenantSlug]/devices/(devices).tsx
@@ -48,38 +48,38 @@ export const route = {
 const column = createColumnHelper<RouterOutput["device"]["list"][number]>();
 
 const columns = [
-	// selectCheckboxColumn,
-	// column.accessor("name", {
-	// 	header: "Name",
-	// 	cell: (props) => (
-	// 		<A
-	// 			class="font-medium hover:underline focus:underline p-1 -m-1 w-full block"
-	// 			href={props.row.original.id}
-	// 		>
-	// 			{props.getValue()}
-	// 		</A>
-	// 	),
-	// }),
-	// column.accessor("os", { header: "Operating System" }),
-	// column.accessor("serialNumber", { header: "Serial Number" }),
-	// column.accessor("owner", {
-	// 	header: "Owner",
-	// 	// TODO: Render as link with the user's name
-	// }),
-	// column.accessor("lastSynced", {
-	// 	header: "Last Synced",
-	// 	cell: (cell) => {
-	// 		const [timeago] = createTimeAgo(cell.getValue());
-	// 		return <p>{timeago()}</p>;
-	// 	},
-	// }),
-	// column.accessor("enrolledAt", {
-	// 	header: "Enrolled At",
-	// 	cell: (cell) => {
-	// 		const [timeago] = createTimeAgo(cell.getValue());
-	// 		return <p>{timeago()}</p>;
-	// 	},
-	// }),
+	selectCheckboxColumn,
+	column.accessor("name", {
+		header: "Name",
+		cell: (props) => (
+			<A
+				class="font-medium hover:underline focus:underline p-1 -m-1 w-full block"
+				href={props.row.original.id}
+			>
+				{props.getValue()}
+			</A>
+		),
+	}),
+	column.accessor("os", { header: "Operating System" }),
+	column.accessor("serialNumber", { header: "Serial Number" }),
+	column.accessor("owner", {
+		header: "Owner",
+		// TODO: Render as link with the user's name
+	}),
+	column.accessor("lastSynced", {
+		header: "Last Synced",
+		cell: (cell) => {
+			const [timeago] = createTimeAgo(cell.getValue());
+			return <p>{timeago()}</p>;
+		},
+	}),
+	column.accessor("enrolledAt", {
+		header: "Enrolled At",
+		cell: (cell) => {
+			const [timeago] = createTimeAgo(cell.getValue());
+			return <p>{timeago()}</p>;
+		},
+	}),
 ];
 
 // TODO: Infinite scroll
@@ -87,171 +87,170 @@ const columns = [
 // TODO: Disable search, filters and sort until all backend metadata has loaded in. Show tooltip so it's clear what's going on.
 
 export default function Page() {
-	// const location = useLocation<{ enrollDialog?: boolean }>();
-	// const tenantSlug = useTenantSlug();
-	// createEffect(() => {
-	// 	console.log("tenantSlug", tenantSlug(), location.state?.enrollDialog);
-	// }); // TODO
+	const location = useLocation<{ enrollDialog?: boolean }>();
+	const tenantSlug = useTenantSlug();
+	createEffect(() => {
+		console.log("tenantSlug", tenantSlug(), location.state?.enrollDialog);
+	}); // TODO
 
-	// const devices = trpc.device.list.createQuery(() => ({
-	// 	tenantSlug: tenantSlug(),
-	// }));
-	// cacheMetadata("device", () => devices.data ?? []);
+	const devices = trpc.device.list.createQuery(() => ({
+		tenantSlug: tenantSlug(),
+	}));
+	cacheMetadata("device", () => devices.data ?? []);
 
-	// const table = createStandardTable({
-	// 	get data() {
-	// 		return devices.data || [];
-	// 	},
-	// 	columns,
-	// });
+	const table = createStandardTable({
+		get data() {
+			return devices.data || [];
+		},
+		columns,
+	});
 
-	// // createSearchParamPagination(table, "page");
-	// createSearchParamFilter(table, "name", "search");
+	// createSearchParamPagination(table, "page");
+	createSearchParamFilter(table, "name", "search");
 
-	return <h1>Hello World</h1>;
-	// return (
-	// 	<PageLayout
-	// 		heading={
-	// 			<div class="flex justify-between w-full">
-	// 				<PageLayoutHeading>Devices</PageLayoutHeading>
-	// 				{/* defaultOpen={location.state?.enrollDialog} */}
-	// 				{/* <Dialog name="todo">
-	// 					<DialogTrigger as={Button}>Enroll</DialogTrigger>
-	// 					<DialogContent>
-	// 						<EnrollDeviceModal />
-	// 					</DialogContent>
-	// 				</Dialog> */}
-	// 			</div>
-	// 		}
-	// 	>
-	// 		<div class="flex flex-row items-center gap-4">
-	// 			{/* <TableSearchParamsInput class="flex-1" query={devices} />
-	// 			<ColumnsDropdown table={table}>
-	// 				<DropdownMenuTrigger
-	// 					as={Button}
-	// 					variant="outline"
-	// 					class="ml-auto select-none"
-	// 				>
-	// 					Columns
-	// 					<IconCarbonCaretDown class="ml-2 h-4 w-4" />
-	// 				</DropdownMenuTrigger>
-	// 			</ColumnsDropdown> */}
-	// 		</div>
-	// 		{/* <Suspense>
-	// 			<StandardTable table={table} />
-	// 			<FloatingSelectionBar table={table} />
-	// 		</Suspense> */}
-	// 	</PageLayout>
-	// );
+	return (
+		<PageLayout
+			heading={
+				<div class="flex justify-between w-full">
+					<PageLayoutHeading>Devices</PageLayoutHeading>
+					{/* defaultOpen={location.state?.enrollDialog} */}
+					<Dialog name="todo">
+						<DialogTrigger as={Button}>Enroll</DialogTrigger>
+						<DialogContent>
+							<EnrollDeviceModal />
+						</DialogContent>
+					</Dialog>
+				</div>
+			}
+		>
+			<div class="flex flex-row items-center gap-4">
+				<TableSearchParamsInput class="flex-1" query={devices} />
+				<ColumnsDropdown table={table}>
+					<DropdownMenuTrigger
+						as={Button}
+						variant="outline"
+						class="ml-auto select-none"
+					>
+						Columns
+						<IconCarbonCaretDown class="ml-2 h-4 w-4" />
+					</DropdownMenuTrigger>
+				</ColumnsDropdown>
+			</div>
+			<Suspense>
+				<StandardTable table={table} />
+				<FloatingSelectionBar table={table} />
+			</Suspense>
+		</PageLayout>
+	);
 }
 
-// const platforms = ["Windows", "Apple", "Android"] as const;
+const platforms = ["Windows", "Apple", "Android"] as const;
 
-// function EnrollDeviceModal() {
-// 	const tenantSlug = useTenantSlug();
-// 	const provider = trpc.tenant.identityProvider.get.createQuery(() => ({
-// 		tenantSlug: tenantSlug(),
-// 	}));
-// 	const users = trpc.user.list.createQuery(
-// 		() => ({
-// 			tenantSlug: tenantSlug(),
-// 		}),
-// 		() => ({
-// 			placeholderData: [],
-// 		}),
-// 	);
+function EnrollDeviceModal() {
+	const tenantSlug = useTenantSlug();
+	const provider = trpc.tenant.identityProvider.get.createQuery(() => ({
+		tenantSlug: tenantSlug(),
+	}));
+	const users = trpc.user.list.createQuery(
+		() => ({
+			tenantSlug: tenantSlug(),
+		}),
+		() => ({
+			placeholderData: [],
+		}),
+	);
 
-// 	const [user, setUser] = createSignal<string>("system");
-// 	const [platform, setPlatform] = createSignal<(typeof platforms)[number]>(
-// 		platforms[0],
-// 	);
+	const [user, setUser] = createSignal<string>("system");
+	const [platform, setPlatform] = createSignal<(typeof platforms)[number]>(
+		platforms[0],
+	);
 
-// 	return (
-// 		<DialogHeader>
-// 			<DialogTitle>Enroll a device</DialogTitle>
-// 			<h2 class="text-foreground text-md font-semibold">
-// 				User-initiated enrollment
-// 			</h2>
-// 			<DialogDescription>
-// 				Your users can go to{" "}
-// 				<A
-// 					href="/enroll"
-// 					target="_blank"
-// 					class="underline"
-// 				>{`${env.VITE_PROD_ORIGIN}/enroll`}</A>{" "}
-// 				and login with{" "}
-// 				<Suspense fallback="their email">
-// 					{match(provider.data?.provider || "entraId")
-// 						.with("entraId", () => "EntraID")
-// 						.exhaustive()}
-// 				</Suspense>{" "}
-// 				to enroll their own devices.
-// 			</DialogDescription>
+	return (
+		<DialogHeader>
+			<DialogTitle>Enroll a device</DialogTitle>
+			<h2 class="text-foreground text-md font-semibold">
+				User-initiated enrollment
+			</h2>
+			<DialogDescription>
+				Your users can go to{" "}
+				<A
+					href="/enroll"
+					target="_blank"
+					class="underline"
+				>{`${env.VITE_PROD_ORIGIN}/enroll`}</A>{" "}
+				and login with{" "}
+				<Suspense fallback="their email">
+					{match(provider.data?.provider || "entraId")
+						.with("entraId", () => "EntraID")
+						.exhaustive()}
+				</Suspense>{" "}
+				to enroll their own devices.
+			</DialogDescription>
 
-// 			<h2 class="text-foreground text-md font-semibold">
-// 				Administrator-initiated enrollment
-// 			</h2>
+			<h2 class="text-foreground text-md font-semibold">
+				Administrator-initiated enrollment
+			</h2>
 
-// 			<DialogDescription>
-// 				Enroll the current device on behalf of another user.
-// 			</DialogDescription>
+			<DialogDescription>
+				Enroll the current device on behalf of another user.
+			</DialogDescription>
 
-// 			<Select
-// 				virtualized
-// 				value={user()}
-// 				onChange={setUser}
-// 				disallowEmptySelection={true}
-// 				options={["system", ...(users.data?.map((user) => user.id) || [])]}
-// 				disabled={!users.data || users.data.length === 0}
-// 				placeholder="Select a user to enroll as..."
-// 				class="flex-1"
-// 			>
-// 				<SelectTrigger aria-label="User to enroll as" class="w-full">
-// 					<SelectValue<string>>
-// 						{(state) =>
-// 							state.selectedOption() === "system"
-// 								? "System"
-// 								: users.data!.find(
-// 										(user) => user.id === state.selectedOption(),
-// 									)!.name
-// 						}
-// 					</SelectValue>
-// 				</SelectTrigger>
-// 				<SelectContentVirtualized
-// 					length={() => (users.data?.length || 0) + 1}
-// 					getItemIndex={(id) =>
-// 						id === "system"
-// 							? 0
-// 							: users.data!.findIndex((user) => user.id === id) + 1
-// 					}
-// 				>
-// 					{(_, i) => (i === 0 ? "System" : users.data![i - 1]!.name)}
-// 				</SelectContentVirtualized>
-// 			</Select>
-// 			<div class="flex w-full">
-// 				<Select
-// 					value={platform()}
-// 					onChange={setPlatform}
-// 					// @ts-expect-error
-// 					options={platforms}
-// 					disallowEmptySelection={true}
-// 					placeholder="Select a platform..."
-// 					itemComponent={(props) => (
-// 						<SelectItem item={props.item}>{props.item.rawValue}</SelectItem>
-// 					)}
-// 					class="flex-1"
-// 				>
-// 					<SelectTrigger aria-label="Platform" class="w-full">
-// 						<SelectValue<string>>
-// 							{(state) => state.selectedOption()}
-// 						</SelectValue>
-// 					</SelectTrigger>
-// 					<SelectContent />
-// 				</Select>
-// 				<Button class="w-full max-w-40 ml-4" onClick={() => alert("TODO")}>
-// 					Enroll
-// 				</Button>
-// 			</div>
-// 		</DialogHeader>
-// 	);
-// }
+			<Select
+				virtualized
+				value={user()}
+				onChange={setUser}
+				disallowEmptySelection={true}
+				options={["system", ...(users.data?.map((user) => user.id) || [])]}
+				disabled={!users.data || users.data.length === 0}
+				placeholder="Select a user to enroll as..."
+				class="flex-1 pb-2"
+			>
+				<SelectTrigger aria-label="User to enroll as" class="w-full">
+					<SelectValue<string>>
+						{(state) =>
+							state.selectedOption() === "system"
+								? "System"
+								: users.data!.find(
+										(user) => user.id === state.selectedOption(),
+									)!.name
+						}
+					</SelectValue>
+				</SelectTrigger>
+				<SelectContentVirtualized
+					length={() => (users.data?.length || 0) + 1}
+					getItemIndex={(id) =>
+						id === "system"
+							? 0
+							: users.data!.findIndex((user) => user.id === id) + 1
+					}
+				>
+					{(_, i) => (i === 0 ? "System" : users.data![i - 1]!.name)}
+				</SelectContentVirtualized>
+			</Select>
+			<div class="flex w-full">
+				<Select
+					value={platform()}
+					onChange={setPlatform}
+					// @ts-expect-error
+					options={platforms}
+					disallowEmptySelection={true}
+					placeholder="Select a platform..."
+					itemComponent={(props) => (
+						<SelectItem item={props.item}>{props.item.rawValue}</SelectItem>
+					)}
+					class="flex-1"
+				>
+					<SelectTrigger aria-label="Platform" class="w-full">
+						<SelectValue<string>>
+							{(state) => state.selectedOption()}
+						</SelectValue>
+					</SelectTrigger>
+					<SelectContent />
+				</Select>
+				<Button class="w-full max-w-40 ml-4" onClick={() => alert("TODO")}>
+					Enroll
+				</Button>
+			</div>
+		</DialogHeader>
+	);
+}
