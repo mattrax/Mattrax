2f2e6d021aa7847e2486d1920d7889977c56d473
Oscar Beaumont <oscar@otbeaumont.me>
2024-06-13 12:49:03 +0800

    hide tos cancel button when in EntraID join mode

--- apps/web/src/app/tos.tsx
+++ apps/web/src/app/tos.tsx
@@ -1,13 +1,21 @@
 import { buttonVariants } from "@mattrax/ui/button";
 import type { APIEvent } from "@solidjs/start/server";
 import clsx from "clsx";
+import { Show } from "solid-js";
 import { renderWithApp } from "~/entry-server";
 import { Layout } from "./enroll/util";
 
 // This is opened in the MDM webview during AzureAD enrollment
 export async function GET({ request, nativeEvent }: APIEvent) {
 	const url = new URL(request.url);
+
+	// Set by MDM webview
 	const redirect_uri = url.searchParams.get("redirect_uri") || "";
+	const mode = url.searchParams.get("mode") || "";
+
+	// > Users skip the Terms of Use when they're adding a Microsoft work account to their device. However, they can't skip it during the Microsoft Entra join process.
+	// https://learn.microsoft.com/en-us/windows/client-management/azure-active-directory-integration-with-mdm
+	const isEntraAdJoinMode = mode === "azureadjoin";
 
 	const params = new URLSearchParams();
 	params.append("IsAccepted", "true");
@@ -25,17 +33,19 @@ export async function GET({ request, nativeEvent }: APIEvent) {
 					<li>View installed software and settings</li>
 				</ul>
 				<div class="flex w-full max-w-sm space-x-2">
-					<a
-						href={`${redirect_uri}?${params.toString()}`}
-						class={clsx(
-							buttonVariants({
-								variant: "outline",
-							}),
-							"flex-1",
-						)}
-					>
-						Cancel
-					</a>
+					<Show when={!isEntraAdJoinMode}>
+						<a
+							href={`${redirect_uri}?${params.toString()}`}
+							class={clsx(
+								buttonVariants({
+									variant: "outline",
+								}),
+								"flex-1",
+							)}
+						>
+							Cancel
+						</a>
+					</Show>
 					<a
 						href={`${redirect_uri}?${params.toString()}`}
 						class={clsx(buttonVariants({}), "flex-1")}
