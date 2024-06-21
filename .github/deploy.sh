43d5f3b2bbb3f8a0defccc19bfa3e915025e722c
Brendan Allan <brendonovich@outlook.com>
2024-06-21 12:37:06 +0800

    remove shebang and skip build

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
 # TODO: Remove this once the proper updater and CI is implemented.
 # Deploy an update to Mattrax.
 
 set -e
 
 export HASH=$(git rev-parse HEAD)
-export CLOUDFLARE_ACCOUNT_ID="f02b3ef168fe64129e9941b4fb2e4dc1" 
+export CLOUDFLARE_ACCOUNT_ID="f02b3ef168fe64129e9941b4fb2e4dc1"
 
-cargo zigbuild --release --target aarch64-unknown-linux-musl -p mattrax
+# cargo zigbuild --release --target aarch64-unknown-linux-musl -p mattrax
 
 wrangler r2 object put "static/mattrax/$HASH/aarch64-unknown-linux" --file=target/aarch64-unknown-linux-musl/release/mattrax --cache-control "public, max-age=31536000, immutable"
 echo "$HASH" | wrangler r2 object put "static/nightly" --pipe
