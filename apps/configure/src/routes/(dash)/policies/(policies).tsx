186415b2f3177e4c5a1f0d854f4851fdb27c4d40
Oscar Beaumont <oscar@otbeaumont.me>
2024-07-26 01:09:45 +0800

    configure: create policy demo

--- apps/configure/src/routes/(dash)/policies/(policies).tsx
+++ apps/configure/src/routes/(dash)/policies/(policies).tsx
@@ -1,7 +1,9 @@
 import { PageLayout, PageLayoutHeading } from "~/components/PageLayout";
 import { SearchPage, createSearchPageContext } from "~/components/search";
+import { useSyncEngine } from "~/lib/sync";
 
 export default function Page() {
+	const sync = useSyncEngine();
 	const ctx = createSearchPageContext([
 		{
 			type: "enum",
@@ -13,6 +15,52 @@ export default function Page() {
 	return (
 		<PageLayout heading={<PageLayoutHeading>Policies</PageLayoutHeading>}>
 			<SearchPage {...ctx} showFilterBar={false} />
+
+			<button
+				type="button"
+				onClick={async () => {
+					const token = await sync.getAccessToken();
+
+					const resp = await fetch(
+						"https://graph.microsoft.com/beta/deviceManagement/configurationPolicies",
+						{
+							method: "POST",
+							headers: {
+								Authorization: token,
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								name: "Created via API",
+								platforms: "windows10",
+								technologies: "mdm",
+								settings: [
+									{
+										"@odata.type":
+											"#microsoft.graph.deviceManagementConfigurationSetting",
+										settingInstance: {
+											"@odata.type":
+												"#microsoft.graph.deviceManagementConfigurationChoiceSettingInstance",
+											settingDefinitionId:
+												"device_vendor_msft_policy_config_accounts_allowaddingnonmicrosoftaccountsmanually",
+											choiceSettingValue: {
+												"@odata.type":
+													"#microsoft.graph.deviceManagementConfigurationChoiceSettingValue",
+												value:
+													"device_vendor_msft_policy_config_accounts_allowaddingnonmicrosoftaccountsmanually_1",
+												children: [],
+											},
+										},
+									},
+								],
+							}),
+						},
+					);
+
+					console.log(await resp.text());
+				}}
+			>
+				Debug
+			</button>
 		</PageLayout>
 	);
 }
