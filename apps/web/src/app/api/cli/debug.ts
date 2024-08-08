17928a89893d65f1f13edb27dbffa26557819910
Oscar Beaumont <oscar@otbeaumont.me>
2024-08-08 16:48:19 +0800

    Debug all possible domains

--- apps/web/src/app/api/cli/debug.ts
+++ apps/web/src/app/api/cli/debug.ts
@@ -21,6 +21,12 @@ export async function GET({ request }: APIEvent) {
 		"https://mdm.mattrax.app/psdb.v1alpha1.Database/Execute",
 	);
 
+	const test2 = await fetch(
+		"https://enterpriseenrollment.mattrax.app/psdb.v1alpha1.Database/Execute",
+	);
+
+	const test3 = await fetch("https://mattrax.fly.io");
+
 	return new Response(
 		JSON.stringify({
 			DATABASE_URL: env.DATABASE_URL,
@@ -28,6 +34,10 @@ export async function GET({ request }: APIEvent) {
 			resultStack,
 			testStatus: test.status,
 			testBody: await test.text(),
+			test2Status: test2.status,
+			test2Body: await test2.text(),
+			test3Status: test3.status,
+			test3Body: await test3.text(),
 		}),
 	);
 }
