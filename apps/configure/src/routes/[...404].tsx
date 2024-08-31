380f65f3c845541999675195a5e960bfd0eb3281
Oscar Beaumont <oscar@otbeaumont.me>
2024-08-31 23:24:52 +0800

    Drop `apps/configure`

--- apps/configure/src/routes/[...404].tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-	return (
-		<div class="p-4 flex flex-col space-y-2">
-			<h1 class="text-2xl">404: Not Found</h1>
-			<p>We could not find the page you requested!</p>
-		</div>
-	);
-}
