ada24a89f1c8cea604ac10e0f9d6d387730ae780
Oscar Beaumont <oscar@otbeaumont.me>
2024-10-28 15:03:10 +0800

    allow mysql in prod for a bit

--- apps/web/src/db/index.ts
+++ apps/web/src/db/index.ts
@@ -11,14 +11,14 @@ export const db = withEnv(() => {
 	const client = new Client({
 		url: env.DATABASE_URL,
 		fetch: async (input, init) => {
-			if (import.meta.env.DEV) {
+			// if (import.meta.env.DEV) {
 				if (env.DATABASE_URL.startsWith("mysql://")) {
 					if (!fetchHandler)
 						fetchHandler = (
 							await import("@mattrax/mysql-planetscale")
 						).createFetchHandler(env.DATABASE_URL);
 				}
-			}
+			// }
 
 			// Cloudflare Workers doesn't support like the `cache` property
 			// biome-ignore lint/performance/noDelete:
