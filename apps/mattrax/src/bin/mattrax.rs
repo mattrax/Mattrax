f80de6941fb32f0c824e575bf8e3561ef17c1d8a
Oscar Beaumont <oscar@otbeaumont.me>
2025-01-01 22:32:26 +0800

    Axum graceful shutdown

--- apps/mattrax/src/bin/mattrax.rs
+++ apps/mattrax/src/bin/mattrax.rs
@@ -1,6 +1,6 @@
 //! The standard entrypoint for Mattrax.
 
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, signal};
 use tracing::{error, info};
 
 #[tokio::main]
@@ -18,8 +18,33 @@ async fn main() {
             listener.local_addr().unwrap_or(args.listen_addr)
         );
         axum::serve(listener, mx_api::mount())
+            .with_graceful_shutdown(shutdown_signal())
             .await
             // I checked and I think this is actually unreachable.
             .expect("Error with Axum server");
+    }
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
     }
 }
